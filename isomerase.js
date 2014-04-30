befb41815408655e4f34c60e0ad0e66b0246594b
Deep Sohelia <soheliadeep@gmail.com>
2014-04-30 18:08:40 +0100

    Update isomerase.js

--- isomerase.js
+++ isomerase.js
@@ -1,7 +1,7 @@
 console.log("Thanks for using Isomerase! For more info, go to https://github.com/isaacrg/isomerase.wiki.git");
 
 var isomer = document.createElement('script');
-isomer.src = 'https://raw.githubusercontent.com/isaacrg/isomerase/master/isomer.min.js';
+isomer.src = 'https://raw.githubusercontent.com/chillibyte/isomerase/master/isomer.min.js';
 document.getElementsByTagName("head")[0].appendChild(isomer);
 
 console.log("Loaded");
