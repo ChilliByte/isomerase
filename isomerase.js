41aa422b9a876bbd94e0098ff92b7505f9e58729
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-04-30 12:50:53 +0100

    Create isomerase.js

---
+++ isomerase.js
@@ -0,0 +1,28 @@
+iso.add(whatevs)
+
+var canvas = new isoCanvas("id");
+
+
+{
+  id:canvas,
+  width:5,
+  hight:5,
+  entities[cube{0,0,0,2,3,4,00ff00},goal[path{[0,0,0],[1,0,0],[1,1,0],[0,1,0]}];
+}
+
+function apple(color) {
+  this.color = color
+}
+
+
+var pinkLady = new apple(pinkyRed);
+alert(pinkLady.color)
+x = is
+
+setInterval(isoRender(canvas,1000),);
+
+if (touching(x,y)){
+
+game.config({
+  keyboard: true;
+});
